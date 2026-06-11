25d8601c4a175f11888d824576c1f0c69e3ff253
egorka44252 <144693055+egorka44252@users.noreply.github.com>
2026-06-12 01:26:54 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'diya-v1';
+const ASSETS = [
+  '/Xysxa-sites/',
+  '/Xysxa-sites/index.html',
+  '/Xysxa-sites/manifest.json',
+  '/Xysxa-sites/assets/styles.css',
+  '/Xysxa-sites/assets/app.js',
+  '/Xysxa-sites/assets/anime.js',
+  '/Xysxa-sites/assets/favicon.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
